3e5f6514540e747992bbda2ff8e646ea77abf1f5
Omelchenko Aleksandr <omelchenkoaleks@gmail.com>
2020-07-27 10:47:58 +0300

    Делаем то же самое получение и сохранение, но уже с помощью lifecycle.

--- app/src/main/java/com/omelchenkoaleks/mvvm/MainActivity.kt
+++ app/src/main/java/com/omelchenkoaleks/mvvm/MainActivity.kt
@@ -3,6 +3,7 @@ package com.omelchenkoaleks.mvvm
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,39 +12,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d(TAG, "onCreate")
-    }
 
-    override fun onStart() {
-        super.onStart()
-        Log.d(TAG, "onStart")
-
-        getData.getData()
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.d(TAG, "onResume")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.d(TAG, "onPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG, "onStop")
-
-        getData.sendData()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG, "onDestroy")
-    }
-
-    companion object {
-        const val TAG = "MyActivity"
+        /*
+            мы может передать экземплят getData, потому что класс реализует
+            интерфейс LifecycleObserver
+         */
+        lifecycle.addObserver(getData)
     }
 }
