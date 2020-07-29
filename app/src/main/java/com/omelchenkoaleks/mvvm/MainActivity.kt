5a9bde47777961a933898a4375749ad3318988f1
Omelchenko Aleksandr <omelchenkoaleks@gmail.com>
2020-07-29 15:14:05 +0300

    Обратный счетчик при смене ориентации начинает отсчет сначала. Вот как сделать, чтобы отсчет продолжался - поможет ViewModel.

--- app/src/main/java/com/omelchenkoaleks/mvvm/MainActivity.kt
+++ app/src/main/java/com/omelchenkoaleks/mvvm/MainActivity.kt
@@ -1,43 +1,27 @@
 package com.omelchenkoaleks.mvvm
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.MediatorLiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Observer
-import androidx.lifecycle.Transformations
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    val liveDataStringOne = MutableLiveData<String>()
-    val liveDataStringTwo = MutableLiveData<String>()
-    val mediatorLiveData = MediatorLiveData<String>()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
-        liveDataStringOne.value = "One"
-        liveDataStringTwo.value = "Two"
+    override fun onStart() {
+        super.onStart()
+        object : CountDownTimer(20000, 1000) {
+            override fun onFinish() {
+            }
 
-        mediatorLiveData.addSource(liveDataStringOne) {
-            text_one_for_liveData.text = it
-        }
-
-        mediatorLiveData.addSource(liveDataStringTwo) {
-            text_two_for_liveData.text = it
-        }
-
-        mediatorLiveData.observe(this, Observer {  })
-
-        button_one_for_liveData.setOnClickListener {
-            liveDataStringOne.value = edit_text_for_liveData.text.toString()
-        }
-
-        button_two_for_liveData.setOnClickListener {
-            liveDataStringTwo.value = edit_text_for_liveData.text.toString()
-        }
+            override fun onTick(millisUntilFinished: Long) {
+                test_text_view.text = (millisUntilFinished / 1000).toString()
+            }
+        }.start()
     }
 
 }
