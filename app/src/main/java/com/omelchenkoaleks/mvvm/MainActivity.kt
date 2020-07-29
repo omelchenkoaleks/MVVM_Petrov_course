4d775722b4f0b4f0106266b5aeb9f417d8272388
Omelchenko Aleksandr <omelchenkoaleks@gmail.com>
2020-07-29 10:49:04 +0300

    LiveData устанавливаем значение в TextView в главном потоке.

--- app/src/main/java/com/omelchenkoaleks/mvvm/MainActivity.kt
+++ app/src/main/java/com/omelchenkoaleks/mvvm/MainActivity.kt
@@ -4,30 +4,39 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.MutableLiveData
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val getData = GetData()
+    // Lifecycle :
+//    private val getData = GetData()
+
+    var liveDataString = MutableLiveData<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        liveDataString.value = "Hello LiveData!"
+        text_for_liveData.text = liveDataString.value
+
+
+
+
+
+
+        // ----- Lifecycle : -----
+
         /*
             мы может передать экземплят getData, потому что класс реализует
             интерфейс LifecycleObserver
          */
-        lifecycle.addObserver(getData)
-        if (lifecycle.currentState == Lifecycle.State.INITIALIZED) {
-            Log.d("TAG", "INITIALIZED")
-            println("INITIALIZED")
-        }
+//        lifecycle.addObserver(getData)
+//        if (lifecycle.currentState == Lifecycle.State.INITIALIZED) {
+//            Log.d("TAG", "INITIALIZED")
+//            text.text = "INITIALIZED"
+//            println("INITIALIZED")
+//        }
      }
-
-    override fun onStart() {
-        if (lifecycle.currentState == Lifecycle.State.STARTED) {
-            Log.d("TAG", "STARTED")
-        }
-        super.onStart()
-    }
 }
