f1f787dc3aa69952b89a58874410e2363ce947a5
Omelchenko Aleksandr <omelchenkoaleks@gmail.com>
2020-07-29 14:17:29 +0300

    Как использовать Transformations.map?

--- app/src/main/java/com/omelchenkoaleks/mvvm/MainActivity.kt
+++ app/src/main/java/com/omelchenkoaleks/mvvm/MainActivity.kt
@@ -1,70 +1,37 @@
 package com.omelchenkoaleks.mvvm
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
+import androidx.lifecycle.Transformations
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
-    // Lifecycle :
-//    private val getData = GetData()
-
-//    var liveDataString = MutableLiveData<String>()
-
-    private val liveData = MyLiveData()
-    lateinit var observer: Observer<String>
+    var liveDataString = MutableLiveData<String>()
+    var liveDataInt = MutableLiveData<Int>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        observer = Observer {
-            text_for_liveData.text = it
+        liveDataString.value = "String"
+        liveDataInt.value = 10_000
+
+
+        text_string_for_liveData.text = liveDataString.value
+        text_int_for_liveData.text = liveDataInt.value.toString()
+
+        button_for_liveData.setOnClickListener {
+            liveDataString = Transformations.map(liveDataInt) {
+                it.toString()
+            } as MutableLiveData<String>
+
+            liveDataString.observe(this, Observer {
+                text_string_for_liveData.text = it
+            })
         }
-
-        button_for_liveData.setOnClickListener{
-            liveData.setValueToLiveData(edit_text_for_liveData.text.toString())
-        }
-
-//        liveData.observe(this, Observer {
-//            text_for_liveData.text = it
-//        })
-
-
-
-
-        // В главном потоке:
-//        liveDataString.value = "Hello LiveData!"
-//        text_for_liveData.text = liveDataString.value
-
-
-        // ----- Lifecycle : -----
-        /*
-            мы можем передать экземплят getData, потому что класс реализует
-            интерфейс LifecycleObserver
-         */
-//        lifecycle.addObserver(getData)
-//        if (lifecycle.currentState == Lifecycle.State.INITIALIZED) {
-//            Log.d("TAG", "INITIALIZED")
-//            text.text = "INITIALIZED"
-//            println("INITIALIZED")
-//        }
-     }
-
-    override fun onStart() {
-        super.onStart()
-        liveData.observe(this, observer)
     }
 
-    override fun onStop() {
-        super.onStop()
-        liveData.removeObserver(observer)
-    }
 }
